93e25baeeeae251444f046c8b993069a45e1ab72
jay-pike <jay.pike12@gmail.com>
2022-02-07 23:03:19 -0500

    activate, calculate, activationderivative methods draft of Neuron class

--- prj1/project1_empty.py
+++ prj1/project1_empty.py
@@ -1,5 +1,7 @@
 import numpy as np
 import sys
+import math
+
 """
 For this entire file there are a few constants:
 activation:
@@ -15,25 +17,59 @@ loss:
 class Neuron:
     #initilize neuron with activation type, number of inputs, learning rate, and possibly with set weights
     def __init__(self,activation, input_num, lr, weights=None):
-        print('constructor')    
+        self.activation = activation
+        self.input_num = input_num
+        self.lr = lr
+
+        # the last value of the weights vector is the bias
+        self.weights = weights if weights is not None else np.random.rand(input_num)
+
+        #initialize inputs and outputs arrays for saving feedforward values for later use in backprop
+        self.inputs = None
+        self.output = None
         
     #This method returns the activation of the net
     def activate(self,net):
-        print('activate')   
+        if self.activation == "logistic":
+            post_activation = 1 / (1 + math.exp(-net))
+
+        elif self.activation == "linear":
+            post_activation = net
+
+        else:
+            print("Unrecognized activation function.")
+
+        return post_activation
         
     #Calculate the output of the neuron should save the input and output for back-propagation.   
-    def calculate(self,input):
-        print('calculate')
+    def calculate(self, inputs):
+        neuron_output = np.sum(np.multiply(self.weights[:-1], inputs)) + self.weights[-1]
+
+        # Save values for use in backprop later
+        self.inputs = inputs
+        self.output = neuron_output
+
+        return neuron_output
+
 
     #This method returns the derivative of the activation function with respect to the net   
     def activationderivative(self):
-        print('activationderivative')   
+        if self.activation == "logistic":
+            act_deriv = self.output * (1 - self.output)
+
+        elif self.activation == "linear":
+            act_deriv = 1
+
+        else:
+            print("Unrecognized activation function.")
+
+        return act_deriv
     
     #This method calculates the partial derivative for each weight and returns the delta*w to be used in the previous layer
     def calcpartialderivative(self, wtimesdelta):
         print('calcpartialderivative') 
     
-    #Simply update the weights using the partial derivatives and the leranring weight
+    #Simply update the weights using the partial derivatives and the learning weight
     def updateweight(self):
         print('updateweight')
 
@@ -79,7 +115,28 @@ class NeuralNetwork:
 
 if __name__=="__main__":
     if (len(sys.argv)<2):
-        print('a good place to test different parts of your code')
+        # Test Code for Neuron Class
+        neuron_test = Neuron("logistic", 4, 0.01, [0.2, 0.3, 0.4, 0.5])
+        #neuron_test = Neuron("logistic", 4, 0.01)
+        print(neuron_test.lr)
+        print(neuron_test.input_num)
+        print(neuron_test.activation)
+        print(neuron_test.weights)
+
+        # Testing the "activate" method
+        act_func_val = neuron_test.activate(5)
+        print(act_func_val)
+
+        # Testing the "calculate" method
+        calc_vect = neuron_test.calculate([1, 2, 3])
+        print(calc_vect)
+        print(neuron_test.inputs)
+        print(neuron_test.output)
+
+        # Testing the "activationderivative" method
+        actderiv_val = neuron_test.activationderivative()
+        print(actderiv_val)
+
         
     elif (sys.argv[1]=='example'):
         print('run example from class (single step)')
@@ -88,7 +145,27 @@ if __name__=="__main__":
         np.array([0.01,0.99])
         
     elif(sys.argv[1]=='and'):
-        print('learn and')
+        print("learn and")
+        and_inputs = np.array([[0, 0],
+                               [0, 1],
+                               [1, 0],
+                               [1, 1]])
+        and_outputs = np.array([[0],
+                                [0],
+                                [0],
+                                [1]])
+        print(and_outputs.shape)
+        print(and_inputs.shape)
         
     elif(sys.argv[1]=='xor'):
         print('learn xor')
+        xor_inputs = np.array([[0, 0],
+                               [0, 1],
+                               [1, 0],
+                               [1, 1]])
+        xor_outputs = np.array([[0],
+                                [1],
+                                [1],
+                                [0]])
+        print(xor_outputs.shape)
+        print(xor_inputs.shape)
