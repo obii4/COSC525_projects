7afe457bcaae67a09c08f685f5f09a93e7e2734e
obii4 <cobrien8@vols.utk.edu>
2022-04-07 13:17:28 -0400

    minor changes

--- prj3/project_3.py
+++ prj3/project_3.py
@@ -3,21 +3,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import classification_report
+
+#monitor image loading loop for some visual feedback
+from tqdm import tqdm
 
 # image processing
 from PIL import Image
 
+import tensorflow as tf
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import SGD
 
+
 # define path to images and label files, the _slim.csv
 # file only contains the features of interest
 image_train_path = '/Users/chrisobrien/Desktop/p3/train/'
-label_train_path = '/Users/chrisobrien/Desktop/grad school/courses/spring 2022/cosc 525/COSC525_projects/prj3/fairface_label_train_slim.csv'
+label_train_path = '/Users/chrisobrien/Desktop/grad school/courses/spring 2022/cosc 525/COSC525_projects/prj3/fairface_label_train_gender.csv'
 
 image_val_path = '/Users/chrisobrien/Desktop/p3/val/'
-label_val_path = '/Users/chrisobrien/Desktop/grad school/courses/spring 2022/cosc 525/COSC525_projects/prj3/fairface_label_val_slim.csv'
+label_val_path = '/Users/chrisobrien/Desktop/grad school/courses/spring 2022/cosc 525/COSC525_projects/prj3/fairface_label_val_gender.csv'
 
 
 # load all of file names to a variable
@@ -58,11 +64,11 @@ def label_encoder(path, path2):
     data_array2 = np.asarray(text)
     #data_array2 = data_array2[0:1000]
 
-    fuck = OneHotEncoder()
-    fuck.fit(data_array)
+    oHenc = OneHotEncoder()
+    oHenc.fit(data_array)
     # print(enc.categories_) #if you want to view the encoded categories
-    train_lab = fuck.transform(data_array).toarray()
-    val_lab = fuck.transform(data_array2).toarray()
+    train_lab = oHenc.transform(data_array).toarray()
+    val_lab = oHenc.transform(data_array2).toarray()
 
     #convert each label to array
     out1 = np.array([np.array(xi) for xi in train_lab])
@@ -98,17 +104,6 @@ class ImageLoader:
         image = np.array(Image.open(path), dtype=float) / 255
         return image
 
-    def load_labels(self, path):
-        '''
-        Loads csv file of labels as np array
-        '''
-        with open(path, 'r') as f:
-            load = csv.reader(f, delimiter=',')
-            next(load)
-            text = [text for text in load]
-        data_array = np.asarray(text)
-        return data_array[0:len(self.file_names)] #change this based off of how many filenames you are loading
-
 
     # load images, masks, and (if applicable) roi's
     def load_data(self, paths=None):
@@ -118,7 +113,7 @@ class ImageLoader:
 
         images = []
         file_names = paths if paths is not None else self.file_names
-        for file_name in file_names:
+        for file_name in tqdm(file_names):
             image_name = self.image_path + file_name
             images.append(self.load_image(image_name))
 
@@ -149,14 +144,15 @@ if __name__ == "__main__":
     # Load and encode labels
     Ytrain, Yval = label_encoder(label_train_path, label_val_path)
 
+    print(Yval.shape[1])
     # Check the shape of everything
     print(f'Xtrain is of shape: {Xtrain.shape}')
     print(f'Ytrain is of shape: {Ytrain.shape}')
-    #print(f'Ytrain has {Ytrain[1].shape} features....')
+    print(f'Ytrain has {Ytrain.shape[1]} features....')
 
     print(f'Xval is of shape: {Xval.shape}')
     print(f'Yval is of shape: {Yval.shape}')
-    #print(f'Yval has {Yval[1].shape} features....')
+    print(f'Yval has {Ytrain.shape[1]} features....')
 
 
 
@@ -166,15 +162,39 @@ if __name__ == "__main__":
     model.add(Dense(1024, input_shape=(1024,), activation="tanh"))
     model.add(Dense(512, activation="sigmoid"))
     model.add(Dense(100, activation="relu"))
-    model.add(Dense(9, activation="softmax"))
+    model.add(Dense(Yval.shape[1], activation="softmax"))
     model.summary()
 
+    #confusion = tf.confusion_matrix(labels=model_, predictions=model, num_classes=num_classes)
+
+    # model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+    #     filepath='/Users/chrisobrien/Desktop/cp.cpkt',
+    #     save_weights_only=True,
+    #     monitor='val_accuracy',
+    #     mode='max',
+    #     save_best_only=True)
+
     # train the model using SGD
-    sgd = SGD(0.0000001)
+    sgd = SGD(0.00001) #0.0000001
     model.compile(loss="categorical_crossentropy", optimizer=sgd,
-                  metrics=["accuracy"])
+                metrics=[tf.keras.metrics.CategoricalAccuracy()])
     H = model.fit(Xtrain, Ytrain, validation_data=(Xval, Yval),
-                  epochs=100, batch_size=640)
+                  epochs=1000, batch_size=640) #callbacks=[model_checkpoint_callback]
+
+    print("[INFO] evaluating network...")
+    predictions = model.predict(Xval, batch_size=640)
+    #print(predictions.shape[1])
+    #print(predictions)
+
+
+    preds = (predictions > 0.5)#.long()
+    #print(preds)
+
+
+    #
+    # u = tf.stack([Yval, predictions], axis=1)
+    # print(u)
+    #print(classification_report(Yval, predictions))
 
 
 
