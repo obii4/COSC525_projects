6a20b8d33ea54cca6954ca48ac3b5fdbdb951560
jay-pike <jay.pike12@gmail.com>
2022-05-17 04:27:48 -0400

    batch=100 input=25 epochs=10000

--- final_prj/GAN_attempt_1_tumors.py
+++ final_prj/GAN_attempt_1_tumors.py
@@ -247,7 +247,7 @@ y=np.concatenate((valid,fake))
 print("Training...")
 
 
-num_epochs = 4000
+num_epochs = 10000
 print(f"num_epochs ={num_epochs}")
 
 losses = np.zeros((num_epochs, 3))
@@ -329,4 +329,4 @@ print("End")
 
 
 # Creates a HDF5 file 'my_model.h5'
-generator = tf.keras.models.save_model(generator, r"C:\Users\jaypi\source\COSC525\COSC525_projects\final_prj\generator_model_colab2.h5")
+generator = tf.keras.models.save_model(generator, r"C:\Users\jaypi\source\COSC525\COSC525_projects\final_prj\generator_model_colab4.h5")
